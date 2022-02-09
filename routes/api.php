822d14f80b20ab3eba0092ae098f05fe38b1fdce
ZaaCoDe <60171098+AhmadSultanMA@users.noreply.github.com>
2022-02-09 13:43:31 +0700

    .

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ use App\Http\Controllers\{
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 Route::post('/register/user',[UserController::class, 'register']);
+Route::get('/kelas',[KelasController::class, 'showKelas']);
 
 });
 
@@ -33,7 +34,6 @@ Route::post('/login',  [UserController::class, 'login'])->name('login');
 Route::get('murid/{kelas}',[MapelController::class, 'showMapel']);
 Route::get('bab/{kelas}/{mapel}',[BabController::class, 'showBab']);
 Route::get('tugas/{kelas}/{mapel}/{bab}',[TugasController::class, 'showTugas']);
-Route::get('/kelas',[KelasController::class, 'showKelas']);
 Route::get('/userProgress',[KelasController::class, 'userProgress']);
 
 Route::get('/admin', "App\Http\Controllers\KelasController@showAdmin");
