e776aadf79b711fd5359a49a052981d652255549
Asady04 <fayyadh.asady04@gmail.com>
2022-04-09 11:53:40 +0700

    completed project

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -33,7 +33,13 @@ const Rute = (value) => {
             element={
               <Navigate
                 replace
-                to={localStorage.getItem('role') === "admin" ? "/admin" : "/login"}
+                to={
+                  localStorage.getItem("role") === "admin"
+                    ? "/admin"
+                    : localStorage.getItem("role") === "user"
+                    ? "/user"
+                    : "/login"
+                }
               />
             }
           />
@@ -71,93 +77,9 @@ const Rute = (value) => {
               <Route path="lesson" element={<Materi />} />
             </Route>
           </Route>
-
         </Route>
       </Routes>
     </BrowserRouter>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" exact>
-    //       <Route
-    //         path=""
-    //         element={
-    //           <Navigate
-    //             replace
-    //             to={
-    //               localStorage.getItem("role") === "admin"
-    //                 ? "/admin-dashboard"
-    //                 : "/home"
-    //             }
-    //           />
-    //         }
-    //       />
-    //       <Route
-    //         path="home"
-    //         element={<Dashboard programs={programs} user={user} />}
-    //       />
-    //       {/* Protected Route After Login */}
-    //       <Route element={<ProtectedLogin />}>
-    //         <Route path="login" element={<Login />} />
-    //         <Route path="register" element={<Register />} />
-    //       </Route>
-
-    //       {/* Galang Dana */}
-    //       <Route path="galang-dana" element={<GalangDana />} />
-
-    //       {/* Protected Route Before Login */}
-    //       <Route element={<ProtectedRoute />}>
-    //         <Route exact path="account" element={<AccountSetting />}>
-    //           <Route path="" element={<Navigate replace to="my-profile" />} />
-    //           <Route path="change-password" element={<ChangePass />} />
-    //           <Route path="my-profile" element={<Profile />} />
-    //         </Route>
-    //       </Route>
-
-    //       {/* Protected Route Only for Admin */}
-    //       <Route element={<ProtectedAdmin />}>
-    //         <Route
-    //           exact
-    //           path="admin-dashboard"
-    //           element={<AdminDashboard user={user} program={program} />}
-    //         >
-    //           <Route path="" element={<Home />} />
-    //           <Route path="user-acc" element={<UserAccounts user={user} />} />
-    //           <Route path="admin-acc" element={<AdminAccounts />} />
-    //           <Route path="add-category" element={<AddKategori />} />
-    //           <Route
-    //             path="list-program"
-    //             element={
-    //               <ListProgram
-    //                 setFilterText={setFilterText}
-    //                 filterText={filterText}
-    //                 setProgram={setProgram}
-    //                 program={program}
-    //                 getProg={getProg}
-    //               />
-    //             }
-    //           />
-    //           <Route path="req-program" element={<ProgramReq />} />
-    //           <Route path="pend-payment" element={<PendingPayment />} />
-    //           <Route path="acc-payment" element={<AcceptedPayment />} />
-    //           <Route path="regadminbantunesiaBykel3" element={<RegAdmin />} />
-    //         </Route>
-    //       </Route>
-
-    //       {/* Details */}
-    //       <Route
-    //         path="/detail/:id"
-    //         element={<DetailDonasi programs={programs} />}
-    //       />
-    //       <Route path="*" element={<NotFound />} />
-
-    //       {/* Payment */}
-    //       <Route
-    //         path="/pembayaran/:id"
-    //         element={<Payment programs={programs} />}
-    //       />
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
   );
 };
 export default Rute;
--- src/url.jsx
+++ src/url.jsx
@@ -1,4 +1,4 @@
-let urlport = 'http://127.0.0.1:8000/api/'
+let urlport = 'https://api-bimbelz.herokuapp.com/api/'
 
 //Auth
 export const urlRegister = urlport + 'register'
